63b918f411bc58259da87f201f116a3b4ebcf1bf
MahnoorMukhtar <mahnoormukhtar519@gmail.com>
2024-07-05 09:32:47 +0500

    server.js file changes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cors({
   origin: ["*"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
