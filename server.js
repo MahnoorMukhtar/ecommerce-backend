8553ef641e8b62b7d5268b13421032bba9d96850
MahnoorMukhtar <mahnoormukhtar519@gmail.com>
2024-07-05 09:40:59 +0500

    frontend vercel link

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
-  origin: ["*"],
+  origin: ["https://ecommerce-react-frontend-sage.vercel.app/", "http://localhost:3000"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
